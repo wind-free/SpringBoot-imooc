75452d87b327aec0037b037315334dd92e9ec207
wind-free <107403865011.com>
2018-06-12 18:00:07 +0800

    热部署+资源属性配置

---
+++ imooc-springboot-starter-master1/src/main/java/com/imooc/ImoocSpringbootStarterMaster1Application.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSpringbootStarterMaster1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoocSpringbootStarterMaster1Application.class, args);
+	}
+}
